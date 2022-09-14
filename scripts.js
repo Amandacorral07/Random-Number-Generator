77c4757837d35c04252bf7c15f81f2cf147ffa1e
amandacorral07 <amandalewis0707@gmail.com>
2022-09-13 20:11:15 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const number = document.querySelector(".number")
+const button = document.querySelector(".generate")
+
+const generateNumber = ()
+// const rand = Math.floor(Math.random() * 10 + 1);
+
+// console.log(rand);
